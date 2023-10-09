14d7b3ea65f3e9568221991b6babd42e996302a3
Tony <tony.liu0112@gmail.com>
2023-10-09 15:34:42 +0800

    remove class for deprecated.

--- src/main/kotlin/com/tony/liu/plugins/gradle/tree/listeners/PreLoadListener.kt
+++ src/main/kotlin/com/tony/liu/plugins/gradle/tree/listeners/PreLoadListener.kt
@@ -1,42 +1,44 @@
-package com.tony.liu.plugins.gradle.tree.listeners
-
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManagerListener
-import com.tony.liu.plugins.gradle.tree.context.FileContext
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.apache.commons.io.FileUtils
-import org.apache.commons.io.filefilter.SuffixFileFilter
-import java.io.File
-
-class PreLoadListener : ProjectManagerListener {
-
-    @OptIn(DelicateCoroutinesApi::class)
-    @Suppress("removal")
-    override fun projectOpened(project: Project) {
-        super.projectOpened(project)
-
-        val basePath = project.basePath
-
-        val iterate = FileUtils.iterateFiles(File(basePath!!), SuffixFileFilter("gradle", "kts"), null)
-
-        while (iterate.hasNext()) {
-            val file = iterate.next()
-            if (file.name.startsWith("settings")) {
-                continue
-            }
-
-            print(file.name)
-
-            GlobalScope.launch {
-                val gradleFile = basePath + File.separator + file.name
-                FileContext.init(gradleFile, "")
-            }
-
-            break
-        }
-
-    }
-
-}
+//package com.tony.liu.plugins.gradle.tree.listeners
+//
+//import com.intellij.openapi.project.Project
+//import com.intellij.openapi.project.ProjectManagerListener
+//import com.tony.liu.plugins.gradle.tree.context.FileContext
+//import kotlinx.coroutines.DelicateCoroutinesApi
+//import kotlinx.coroutines.GlobalScope
+//import kotlinx.coroutines.launch
+//import org.apache.commons.io.FileUtils
+//import org.apache.commons.io.filefilter.SuffixFileFilter
+//import java.io.File
+//
+//@Suppress("DEPRECATION")
+//class PreLoadListener : ProjectManagerListener {
+//
+//    @Deprecated("Deprecated in Java")
+//    @OptIn(DelicateCoroutinesApi::class)
+//    @Suppress("removal")
+//    override fun projectOpened(project: Project) {
+//        super.projectOpened(project)
+//
+//        val basePath = project.basePath
+//
+//        val iterate = FileUtils.iterateFiles(File(basePath!!), SuffixFileFilter("gradle", "kts"), null)
+//
+//        while (iterate.hasNext()) {
+//            val file = iterate.next()
+//            if (file.name.startsWith("settings")) {
+//                continue
+//            }
+//
+//            print(file.name)
+//
+//            GlobalScope.launch {
+//                val gradleFile = basePath + File.separator + file.name
+//                FileContext.init(gradleFile, "")
+//            }
+//
+//            break
+//        }
+//
+//    }
+//
+//}
