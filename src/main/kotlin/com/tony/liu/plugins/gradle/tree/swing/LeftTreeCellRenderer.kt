ba0d911b18630a36a86c47ffc1a3a4bc8cb611b1
Tony <tony.liu0112@gmail.com>
2023-12-05 17:17:04 +0800

    兼容231 - 233版本，使用了部分被移除的API

--- src/main/kotlin/com/tony/liu/plugins/gradle/tree/swing/LeftTreeCellRenderer.kt
+++ src/main/kotlin/com/tony/liu/plugins/gradle/tree/swing/LeftTreeCellRenderer.kt
@@ -1,7 +1,8 @@
 package com.tony.liu.plugins.gradle.tree.swing
 
 import com.intellij.ui.JBColor
-import com.intellij.util.ui.StartupUiUtil.isDarkTheme
+import com.intellij.util.ui.StartupUiUtil.isUnderDarcula
+import com.intellij.util.ui.UIUtil.isUnderDarcula
 import com.tony.liu.plugins.gradle.tree.context.FileContext
 import com.tony.liu.plugins.gradle.tree.utils.NodeTextUtils
 import org.apache.commons.lang3.StringUtils
@@ -75,7 +76,7 @@ class LeftTreeCellRenderer(private val searchKey: String, private val dir: Strin
             // 搜索渲染
             setBackgroundSelectionColor(DefaultTreeCellRenderer().backgroundSelectionColor)
 
-            if (isDarkTheme) {
+            if (isUnderDarcula()) {
                 setBackgroundNonSelectionColor(filterSelectColor_Dark)
                 setBackground(filterSelectColor_Dark)
             } else {
@@ -92,7 +93,7 @@ class LeftTreeCellRenderer(private val searchKey: String, private val dir: Strin
         }
 
         // 主题色渲染
-        if (isDarkTheme) {
+        if (isUnderDarcula) {
             setForeground(foreground)
             setBorderSelectionColor(null)
         } else {
@@ -116,7 +117,7 @@ class LeftTreeCellRenderer(private val searchKey: String, private val dir: Strin
     }
 
     private fun setForegroundByScope(nodeText: String): Boolean {
-        val scopeColors = if (isDarkTheme) scopeColorList_Dark else scopeColorList_Light
+        val scopeColors = if (isUnderDarcula) scopeColorList_Dark else scopeColorList_Light
 
         for (scopeColor in scopeColors) {
             for (scope in scopeColor.scops) {
